11968da329c0512c9bea5a7ac710ba56463e8a15
VPKSoft <vpksoft@vpksoft.net>
2019-05-04 20:00:14 +0300

    Initial file commit.

---
+++ ScintillaSpellCheck.cs
@@ -0,0 +1,482 @@
+﻿#region License
+/*
+MIT License
+
+Copyright(c) 2019 Petteri Kautonen
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+*/
+#endregion
+
+using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Windows.Forms;
+using ScintillaNET;
+using WeCantSpell.Hunspell;
+
+namespace VPKSoft.ScintillaSpellCheck
+{
+    /// <summary>
+    /// A class the spell check a <see cref="Scintilla"/> instance.
+    /// Implements the <see cref="System.IDisposable" />
+    /// </summary>
+    /// <seealso cref="System.IDisposable" />
+    public class ScintillaSpellCheck: IDisposable
+    {
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Scintilla"/> class.
+        /// </summary>
+        /// <param name="scintilla">The scintilla to spell check for.</param>
+        /// <param name="dictionaryFile">The dictionary file.</param>
+        /// <param name="affixFile">The affix file.</param>
+        public ScintillaSpellCheck(Scintilla scintilla, string dictionaryFile, string affixFile)
+        {
+            this.scintilla = scintilla;
+
+            this.scintilla.MouseDown += Scintilla_MouseDown;
+
+            LoadDictionary(dictionaryFile, affixFile);
+
+            // set the default indicator..
+            SetIndicator();
+        }
+
+        /// <summary>
+        /// Handles the MouseDown event of the Scintilla control.
+        /// </summary>
+        /// <param name="sender">The source of the event.</param>
+        /// <param name="e">The <see cref="MouseEventArgs"/> instance containing the event data.</param>
+        private void Scintilla_MouseDown(object sender, MouseEventArgs e)
+        {
+            // the right button was clicked..
+            if (e.Button == MouseButtons.Right)
+            {
+                var point = e.Location;
+
+                // get the character index at the click location..
+                int charPosition = scintilla.CharPositionFromPoint(point.X, point.Y);
+
+                // validate the character index and the indicator style at the location..
+                if (charPosition != -1 && scintilla.IndicatorOnFor(ScintillaIndicatorIndex, charPosition))
+                {
+                    // get the word start..
+                    int start = scintilla.WordStartPosition(charPosition, OnlyWordCharacters);
+
+                    // ..and end positions..
+                    int end = scintilla.WordEndPosition(charPosition, OnlyWordCharacters);
+
+                    // this is a deal breaker..
+                    if (start == end)
+                    {
+                        // clean the previous menu (dispose)..
+                        CleanPreviousSuggestMenu();
+                        return;
+                    }
+
+                    // get the word at the mouse click location..
+                    string word = scintilla.Text.Substring(start, end - start);
+
+                    // get the dictionary suggestions..
+                    var suggestions = Dictionary.Suggest(word).ToList();
+
+                    // only create a menu if there are any suggestions..
+                    if (suggestions.Count > 0)
+                    {
+                        // clean the previous menu (dispose)..
+                        CleanPreviousSuggestMenu();
+
+                        PreviousSuggestMenu = CreateSuggestionMenu(suggestions, point, (start, end));
+                    }
+                    else // don't leave the user without a menu..
+                    {
+                        // clean the previous menu (dispose)..
+                        CleanPreviousSuggestMenu();                        
+                    }
+                }
+                else
+                {
+                    // clean the previous menu (dispose)..
+                    CleanPreviousSuggestMenu();
+                }
+            }
+            else
+            {
+                // clean the previous menu (dispose)..
+                CleanPreviousSuggestMenu();
+            }
+        }
+
+        /// <summary>
+        /// Cleans and disposes of the the previous suggest menu.
+        /// </summary>
+        private void CleanPreviousSuggestMenu()
+        {
+            // only do this if assigned..
+            if (PreviousSuggestMenu != null)
+            {
+                // loop through the drop down items and unsubscribe the click events..
+                foreach (ToolStripMenuItem item in PreviousSuggestMenu.Items)
+                {
+                    item.Click -= OnSuggestMenuClick;
+                }
+
+                // clear the items after the event un-subscription..
+                PreviousSuggestMenu.Items.Clear();
+
+                // dispose of the previous suggest menu..
+                using (PreviousSuggestMenu)
+                {
+                    // assign a null value for comparison within the class..
+                    PreviousSuggestMenu = null;
+                }
+            }
+
+            // set the Scintilla's previous context menu back in place..
+            scintilla.ContextMenuStrip = PreviousScintillaContextMenu;
+        }
+
+        /// <summary>
+        /// Gets or sets the previous suggest menu.
+        /// </summary>
+        private ContextMenuStrip PreviousSuggestMenu { get; set; }
+
+        /// <summary>
+        /// Gets or sets the previous context menu of the <see cref="Scintilla"/>.
+        /// </summary>
+        private ContextMenuStrip PreviousScintillaContextMenu { get; set; }
+
+        /// <summary>
+        /// Creates a new suggestion menu for words.
+        /// </summary>
+        /// <param name="suggestions">A collection of suggestions to show to the user.</param>
+        /// <param name="point">The point where the menu should be shown at.</param>
+        /// <param name="pos">The position of the word for suggest alternatives to.</param>
+        /// <returns>A <see cref="ContextMenuStrip"/> containing suggestions for the word.</returns>
+        private ContextMenuStrip CreateSuggestionMenu(List<string> suggestions, Point point, (int start, int end) pos)
+        {
+            // clean the previous menu (dispose)..
+            CleanPreviousSuggestMenu();
+            
+            // create a new ContextMenuStrip class instance..
+            ContextMenuStrip suggestMenu = new ContextMenuStrip();
+
+            // loop through the suggestions..
+            foreach (var suggestion in suggestions)
+            {
+                // ..and create ToolStripMenuItem's out of them..
+                suggestMenu.Items.Add(new ToolStripMenuItem(suggestion, null, OnSuggestMenuClick)
+                    {Tag = pos}); // save the position to the tag..
+            }
+
+            // save the previous context menu of the Scintilla control so it can be restored..
+            PreviousScintillaContextMenu = scintilla.ContextMenuStrip;
+
+            // set the just created context menu as a new context menu for the Scintilla control..
+            scintilla.ContextMenuStrip = suggestMenu;
+
+            // display the menu immediately at the given location..
+            suggestMenu.Show(scintilla, point);
+
+            // return the created context menu..
+            return suggestMenu;
+        }
+
+        /// <summary>
+        /// Handles the <see cref="E:SuggestMenuClick" /> event.
+        /// </summary>
+        /// <param name="sender">The sender of the event.</param>
+        /// <param name="e">The <see cref="EventArgs"/> instance containing the event data.</param>
+        private void OnSuggestMenuClick(object sender, EventArgs e)
+        
+        {
+            // get the clicked menu item from the suggest a word context menu..
+            var clickedItem = (ToolStripMenuItem) sender;
+
+            // get the word position that the suggestion menu replaces..
+            var wordPos = ((int start, int end)) clickedItem.Tag;
+
+            // select the miss-spelled word..
+            scintilla.SelectionStart = wordPos.start;
+            scintilla.SelectionEnd = wordPos.end;
+
+            // replace the miss-spelled word with user "input"..
+            scintilla.ReplaceSelection(clickedItem.Text);
+
+            // clean the previous menu (dispose)..
+            CleanPreviousSuggestMenu();
+        }
+
+        /// <summary>
+        /// Gets or sets the dictionary that the <see cref="WeCantSpell.Hunspell"/> class library has created.
+        /// </summary>
+        private WordList Dictionary { get; set; }
+
+        /// <summary>
+        /// Loads the dictionary in to the <see cref="Dictionary"/> property.
+        /// </summary>
+        /// <param name="dictionaryFile">The dictionary file (*.dic).</param>
+        /// <param name="affixFile">The affix file (*.aff).</param>
+        public void LoadDictionary(string dictionaryFile, string affixFile)
+        {
+            // read the data from the streams and dispose of them..
+            using (var dictionaryStream = File.OpenRead(dictionaryFile))
+            {
+                using (var affixStream = File.OpenRead(affixFile))
+                {
+                    // create a new dictionary..
+                    Dictionary = WordList.CreateFromStreams(dictionaryStream, affixStream);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Sets the <see cref="Scintilla"/> indicator for underlining the miss-spelled words.
+        /// </summary>
+        private void SetIndicator()
+        {
+            // if the current indicator is different from the class indicator property index..
+            if (scintilla.IndicatorCurrent != ScintillaIndicatorIndex)
+            {
+                // ..save the current indicator for to be able to set it back to it's previous value..
+                ScintillaIndicatorCurrent = scintilla.IndicatorCurrent;
+            }
+
+            // set the styles from the public properties--
+            scintilla.Indicators[ScintillaIndicatorIndex].Style = ScintillaIndicatorStyle;
+            scintilla.Indicators[ScintillaIndicatorIndex].ForeColor = ScintillaIndicatorColor;
+
+            // set the current indicator value (0-31)..
+            scintilla.IndicatorCurrent = ScintillaIndicatorIndex;
+        }
+
+        /// <summary>
+        /// A field where the Scintilla instance is saved from the constructor.
+        /// </summary>
+        private readonly Scintilla scintilla;
+
+        /// <summary>
+        /// A field indicating whether the <see cref="Next"/> method call has reached the documents ending.
+        /// </summary>
+        private bool endReached;
+
+        /// <summary>
+        /// The scintilla indicator index field for the <see cref="ScintillaIndicatorIndex"/> property.
+        /// </summary>
+        private int scintillaIndicatorIndex = 31; // set to the maximum to not to override the other indicators hopefully..
+
+        /// <summary>
+        /// Gets or sets the index of the <see cref="Scintilla"/> indicator. The value must be within range of 0 to 31.
+        /// </summary>
+        /// <value>The index of the scintilla indicator.</value>
+        public int ScintillaIndicatorIndex
+        {
+            get => scintillaIndicatorIndex; // just return the value..
+
+            set
+            {
+                // save the property value..
+                scintillaIndicatorIndex = value;
+
+                // reset the Scintilla indicator for underlining the miss-spelled words..
+                ClearSpellCheck();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets the previous word start with the spell checking.
+        /// </summary>
+        private int PreviousWordStart { get; set; } = -1;
+
+        /// <summary>
+        /// Gets or sets the next position to continue the spell checking from.
+        /// </summary>
+        private int NextPosition { get; set; }
+
+        /// <summary>
+        /// Gets or sets the current <see cref="Scintilla"/> indicator index for restoring it after spell checking has been done.
+        /// </summary>
+        private int ScintillaIndicatorCurrent { get; set; }
+
+        /// <summary>
+        /// A field to hold the color for the indicator for a miss-spelled word in the <see cref="Scintilla"/>.
+        /// </summary>
+        private Color scintillaIndicatorColor = Color.Red;
+
+        /// <summary>
+        /// Gets or sets the color for the indicator for a miss-spelled word in the <see cref="Scintilla"/>.
+        /// </summary>
+        public Color ScintillaIndicatorColor 
+        {
+            get => scintillaIndicatorColor; // just return the value..
+
+            set
+            {
+                // set the value..
+                scintillaIndicatorColor = value;
+
+                // reset the Scintilla indicator for underlining the miss-spelled words..
+                ClearSpellCheck();
+            }
+        }
+
+        /// <summary>
+        /// Gets or sets a value indicating whether to to stop searching at the first non-word character regardless of whether the search started at a word or non-word character or
+        /// to use the first character in the search as a word or non-word indicator and then search for that word or non-word boundary.
+        /// </summary>
+        // Comment (C): ScintillaNET..
+        public bool OnlyWordCharacters { get; set; } = true;
+
+        /// <summary>
+        /// Clears the spell check indicator from the <see cref="Scintilla"/> control.
+        /// </summary>
+        public void ClearSpellCheck()
+        {
+            SetIndicator();
+            scintilla.IndicatorClearRange(0, scintilla.TextLength);
+            scintilla.IndicatorCurrent = ScintillaIndicatorCurrent;
+        }
+
+        /// <summary>
+        /// A field holding the <see cref="ScintillaIndicatorStyle"/> property value.
+        /// </summary>
+        private IndicatorStyle scintillaIndicatorStyle = IndicatorStyle.Squiggle;
+
+        /// <summary>
+        /// Gets or sets the <see cref="Scintilla"/> indicator style which is the visual appearance of an indicator in the <see cref="Scintilla"/> control.
+        /// </summary>
+        public IndicatorStyle ScintillaIndicatorStyle 
+        { 
+            get => scintillaIndicatorStyle; // just return the value..
+
+            set
+            {
+                // set the value..
+                scintillaIndicatorStyle = value;
+
+                // reset the Scintilla indicator for underlining the miss-spelled words..
+                ClearSpellCheck();
+            }
+        }
+
+        /// <summary>
+        /// Resets the spell checking position of this instance.
+        /// </summary>
+        public void Reset()
+        {
+            // set the previous word start to the class instance creation default..
+            PreviousWordStart = -1; 
+
+            // set the next word position to the class instance creation default..
+            NextPosition = 0;
+
+            // re-set the indicator for underlining the miss-spelled words..
+            SetIndicator();
+        }
+
+        /// <summary>
+        /// Marks miss-spelled words of the <see cref="Scintilla"/> control.
+        /// </summary>
+        public void SpellCheckScintilla()
+        {
+            // first re-set the search..
+            Reset();
+
+            // create a variable for the next word..
+            (int start, int end, int length, string word) word;
+
+            // loop while a word which spelling to is found..
+            while ((word = Next()) != default)
+            {
+                // if the WeCantSpell.Hunspell disagrees with the words spelling..
+                if (!Dictionary.Check(word.word))
+                {
+                    // ..mark it with an indicator..
+                    scintilla.IndicatorFillRange(word.start, word.end - word.start);
+                }
+            }
+
+            // re-set the saved indicator value to the Scintilla instance..
+            scintilla.IndicatorCurrent = ScintillaIndicatorCurrent;
+        }
+
+        /// <summary>
+        /// Gets a next word in the <see cref="Scintilla"/> class instance to check for miss-spelling.
+        /// </summary>
+        /// <returns>(System.Int32 start, System.Int32 end, System.Int32 length, System.String word).</returns>
+        (int start, int end, int length, string word) Next()
+        {
+            // if the end has already been reached within the Scintilla document..
+            if (endReached)
+            {
+                // ..just return with a default(T)..
+                return default;
+            }
+
+            // loop through the words within the Scintilla instance..
+            for (int i = NextPosition; i < scintilla.TextLength; i++)
+            {
+                // get the word starting position at a given position..
+                int start = scintilla.WordStartPosition(i, OnlyWordCharacters);
+
+                // if the found word index differs from the previous occurrence..
+                if (start != PreviousWordStart)
+                {
+                    // save the value to the previous occurrence value..
+                    PreviousWordStart = start;
+
+                    // get the ending position of the word..
+                    int end = scintilla.WordEndPosition(i, OnlyWordCharacters);
+
+                    // some validation in case a recursion is required..
+                    if ((end == 0 && PreviousWordStart == -1) || start == end)
+                    {
+                        NextPosition++; // ..we must advance..
+                        return Next(); // call your self..
+                    }
+
+                    // set the next position to the found word's ending position..
+                    NextPosition = end; 
+
+                    // return the found word and its position and length..
+                    return (start, end, end - start, scintilla.Text.Substring(start, end - start));
+                }
+            }
+
+            // if nothing was returned withing the loop, assume that the end has been reached..
+            endReached = true;
+
+            // return the default(T)..
+            return default;
+        }
+
+        /// <summary>
+        /// Performs application-defined tasks associated with freeing, releasing, or resetting unmanaged resources.
+        /// </summary>
+        public void Dispose()
+        {
+            scintilla.MouseDown -= Scintilla_MouseDown;
+
+            // clean the previous menu (dispose)..
+            CleanPreviousSuggestMenu();
+        }
+    }
+}
---
+++ TestApp/FormTestApp.Designer.cs
@@ -0,0 +1,115 @@
+﻿namespace TestApp
+{
+    partial class FormTestApp
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTestApp));
+            this.scintilla = new ScintillaNET.Scintilla();
+            this.odAnyFile = new System.Windows.Forms.OpenFileDialog();
+            this.mnuMain = new System.Windows.Forms.MenuStrip();
+            this.mnuFile = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuOpen = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuSpellCheck = new System.Windows.Forms.ToolStripMenuItem();
+            this.mnuMain.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // scintilla
+            // 
+            this.scintilla.Location = new System.Drawing.Point(12, 45);
+            this.scintilla.Name = "scintilla";
+            this.scintilla.Size = new System.Drawing.Size(776, 393);
+            this.scintilla.TabIndex = 0;
+            this.scintilla.Text = "scintilla1";
+            // 
+            // odAnyFile
+            // 
+            this.odAnyFile.FileName = "openFileDialog1";
+            this.odAnyFile.Filter = "All files|*.*";
+            // 
+            // mnuMain
+            // 
+            this.mnuMain.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuFile});
+            this.mnuMain.Location = new System.Drawing.Point(0, 0);
+            this.mnuMain.Name = "mnuMain";
+            this.mnuMain.Size = new System.Drawing.Size(800, 24);
+            this.mnuMain.TabIndex = 1;
+            this.mnuMain.Text = "menuStrip1";
+            // 
+            // mnuFile
+            // 
+            this.mnuFile.DropDownItems.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.mnuOpen,
+            this.mnuSpellCheck});
+            this.mnuFile.Name = "mnuFile";
+            this.mnuFile.Size = new System.Drawing.Size(37, 20);
+            this.mnuFile.Text = "File";
+            // 
+            // mnuOpen
+            // 
+            this.mnuOpen.Name = "mnuOpen";
+            this.mnuOpen.Size = new System.Drawing.Size(215, 22);
+            this.mnuOpen.Text = "Open";
+            this.mnuOpen.Click += new System.EventHandler(this.MnuOpen_Click);
+            // 
+            // mnuSpellCheck
+            // 
+            this.mnuSpellCheck.Name = "mnuSpellCheck";
+            this.mnuSpellCheck.Size = new System.Drawing.Size(215, 22);
+            this.mnuSpellCheck.Text = "Spell check with dictionary";
+            this.mnuSpellCheck.Click += new System.EventHandler(this.MnuSpellCheck_Click);
+            // 
+            // FormTestApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.scintilla);
+            this.Controls.Add(this.mnuMain);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MainMenuStrip = this.mnuMain;
+            this.Name = "FormTestApp";
+            this.Text = "A test application for the VPKSoft.ScintillaSpellCheck library.";
+            this.mnuMain.ResumeLayout(false);
+            this.mnuMain.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private ScintillaNET.Scintilla scintilla;
+        private System.Windows.Forms.OpenFileDialog odAnyFile;
+        private System.Windows.Forms.MenuStrip mnuMain;
+        private System.Windows.Forms.ToolStripMenuItem mnuFile;
+        private System.Windows.Forms.ToolStripMenuItem mnuOpen;
+        private System.Windows.Forms.ToolStripMenuItem mnuSpellCheck;
+    }
+}
+
